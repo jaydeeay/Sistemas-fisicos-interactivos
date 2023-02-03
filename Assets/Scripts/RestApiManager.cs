081e6f7eaba8d7671931e2c0597b9e3e20655de8
jaydeeay <jarroyavep@outlook.com>
2023-02-03 10:39:35 -0500

    update

--- Assets/Scripts/RestApiManager.cs
+++ Assets/Scripts/RestApiManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
 
 public class RestApiManager : MonoBehaviour
 {
